eabbe3423721887ed30546392b1f23383d666704
DeZhaysun <shuaihangtang@gmail.com>
2022-07-04 15:18:43 -0400

    Finalized html/css and working slide bar

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const container = document.querySelector('.container');
 
-const arr = [];
-let gridSize = 64;
+let gridSize = 16;
+setGrid();
 
-for (let i = 0; i < gridSize; i++){
-  const arr1 = [];
-  for (let j = 0; j < gridSize; j++){
-    arr1[j] = document.createElement('div');
-    arr1[j].classList.add('grid');
-    container.appendChild(arr1[j]);
-    let height = 50/gridSize;
-    console.log(height);
-    arr1[j].style.cssText = `height: ${height}vh; width: ${height}vh;`;
+function setGrid(){
+  container.innerHTML = '';
+
+  for (let i = 0; i < gridSize; i++){
+    const arr1 = [];
+    for (let j = 0; j < gridSize; j++){
+      arr1[j] = document.createElement('div');
+      arr1[j].classList.add('grid');
+      container.appendChild(arr1[j]);
+      let height = 50/gridSize;
+      console.log(height);
+      arr1[j].style.cssText = `height: ${height}vh; width: ${height}vh;`;
+    }
   }
-  arr[i] = arr1;
 }
 
 var slider = document.getElementById("myRange");
@@ -23,4 +26,6 @@ output.innerHTML = "Size: " + slider.value.toString(); // Display the default sl
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = "Size: " + slider.value.toString();
+  gridSize = slider.value;
+  setGrid();
 }
